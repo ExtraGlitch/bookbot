e3b58cf061a4d49832affc075e879ccf5bb0bea1
ExtraGlitch <florent-melin@outlook.com>
2025-09-24 23:22:09 +0200

    L1: Print a Report

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from stats import get_num_words
 from stats import get_num_characters
+from stats import sort_list_asc
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = len(get_num_words(text))
-    print(f"Found {num_words} total words")
     num_characters = get_num_characters(text)
-    print(num_characters)
+    sorted_characters = sort_list_asc(num_characters)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for elem in sorted_characters:
+        print(f"{elem["str"]}: {elem["num"]}")
+    print("============= END ===============")
 
 def get_book_text(filePath):
     with open(filePath) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+def sort_on(items):
+    return items["num"]
 
 def get_num_words(text):
     return text.split()
@@ -11,3 +13,14 @@ def get_num_characters(text):
         else:
             characters[character] += 1
     return characters
+
+def sort_list_asc(list_to_sort):
+    sorted_dico = []
+    for elem in list_to_sort:
+        if elem.isalpha():
+            char = {}
+            char["str"] = elem
+            char["num"] = list_to_sort[elem]
+            sorted_dico.append(char)
+    sorted_dico.sort(reverse=True, key=sort_on)
+    return sorted_dico
